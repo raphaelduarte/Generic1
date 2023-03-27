016bf0abf190b5cfa579273490dd0bfd4a88aa23
Raphael Duarte <raphaelduartessph@gmail.com>
2023-03-27 17:02:18 -0300

    Add project files.

---
+++ Generic1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Xml;
+
+namespace Generic1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService printService = new PrintService();
+            Console.WriteLine("How many values?");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                printService.AddValue(x);
+            }
+            printService.Print();
+            Console.WriteLine("First: " + printService.First());
+        }
+    }
+
+
+}
